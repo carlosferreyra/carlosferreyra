f1db42db65eadfbe12404e9258c86a23d27f2923
carlosferreyra <eduferreyraok@gmail.com>
2025-01-21 23:27:39 +0000

    [bot] Update repo with latest info

--- src/projects.js
+++ src/projects.js
@@ -2,6 +2,7 @@ const projects =
 
 [
   {
+    "repo": "https://github.com/carlosferreyra/alura-encriptador",
     "name": "alura-encriptador",
     "img": "https://repository-images.githubusercontent.com/819610263/df6a6b16-c04a-4bfd-a983-43723c86ae05",
     "description": "Challenge de la etapa \"Principiante en Programacion\" en ONE G7.",
@@ -13,6 +14,7 @@ const projects =
     "demo": "https://carlosferreyra.github.io/alura-encriptador/"
   },
   {
+    "repo": "https://github.com/carlosferreyra/alura-monedas",
     "name": "alura-monedas",
     "img": "https://opengraph.githubassets.com/aafe8c8f4cb477d95215dca79e47f3453a11eacdce523cb8c68a8428bab8f3e4/carlosferreyra/alura-monedas",
     "description": "Primer Challenge de la etapa 3 \"Especializacion Backend\" en ONE G7.",
@@ -24,6 +26,7 @@ const projects =
     "demo": ""
   },
   {
+    "repo": "https://github.com/Seminario-Integrador-2024/GestionTUP",
     "name": "GestionTUP",
     "img": "https://repository-images.githubusercontent.com/839530563/77ec1cfb-4a65-4e34-8641-2fa6d5ea3f18",
     "description": "Repositorio Frontend del Proyecto",
@@ -36,6 +39,7 @@ const projects =
     "demo": "https://gestiontup-1.onrender.com"
   },
   {
+    "repo": "https://github.com/Seminario-Integrador-2024/.github",
     "name": ".github",
     "img": "https://opengraph.githubassets.com/45ec1c92c1ab6792053b184089568416b20c58f497cafee32038eec7be2dbd03/Seminario-Integrador-2024/.github",
     "description": "Repositorio de la Organizacion, cambios sobre la misma se veran reflejados en la pag principal",
@@ -43,6 +47,7 @@ const projects =
     "demo": ""
   },
   {
+    "repo": "https://github.com/Kitsune-Studios/WA-Bot-Template",
     "name": "WA-Bot-Template",
     "img": "https://opengraph.githubassets.com/9729a35fbfa3f48bcd05b0094b01171977603e3b6ed5350143eff52db99062eb/Kitsune-Studios/WA-Bot-Template",
     "description": "Standard WhatsApp template for chatbots",
@@ -50,6 +55,7 @@ const projects =
     "demo": ""
   },
   {
+    "repo": "https://github.com/Kitsune-Studios/.github",
     "name": ".github",
     "img": "https://opengraph.githubassets.com/97f3c1f0346e80b9ab959aa2b62702df6f0e3588904b25da66112e863e8c91b6/Kitsune-Studios/.github",
     "description": "Main repository for the organization",
