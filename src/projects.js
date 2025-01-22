444e9a5160962a855426bfa16f34d70e90d6cae1
carlosferreyra <eduferreyraok@gmail.com>
2025-01-22 21:38:43 +0000

    [bot] Update repo with latest info

--- src/projects.js
+++ src/projects.js
@@ -1,29 +1,5 @@
 const projects = [
   {
-    "repo": "https://github.com/carlosferreyra/alura-encriptador",
-    "name": "alura-encriptador",
-    "img": "https://repository-images.githubusercontent.com/819610263/df6a6b16-c04a-4bfd-a983-43723c86ae05",
-    "description": "Challenge de la etapa \"Principiante en Programacion\" en ONE G7.",
-    "stack": [
-      "alura-latam",
-      "one-g7",
-      "oracle"
-    ],
-    "demo": "https://carlosferreyra.github.io/alura-encriptador/"
-  },
-  {
-    "repo": "https://github.com/carlosferreyra/carlosferreyra",
-    "name": "carlosferreyra",
-    "img": "https://repository-images.githubusercontent.com/802189668/b592375d-e8be-4271-874f-6dbf40cbe433",
-    "description": "My GitHub Profile",
-    "stack": [
-      "profile",
-      "profile-readme",
-      "resume"
-    ],
-    "demo": "https://carlosferreyra.me/"
-  },
-  {
     "repo": "https://github.com/Seminario-Integrador-2024/GestionTUP",
     "name": "GestionTUP",
     "img": "https://repository-images.githubusercontent.com/839530563/77ec1cfb-4a65-4e34-8641-2fa6d5ea3f18",
