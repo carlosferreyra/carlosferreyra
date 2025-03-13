26f2efb572ffe074dd5516efc7294e788c3ca3aa
Carlos Eduardo Ferreyra <eduferreyraok@gmail.com>
2025-03-13 11:27:49 -0300

    feat: enhance portfolio functionality with new repository filters and API integration

--- src/projects.js
+++ src/projects.js
@@ -34,21 +34,6 @@ const projects = [
     "demo": "https://www.youtube.com/channel/UCKvwPt6BifPP54yzH99ff1g?view_as=subscriber"
   },
   {
-    "repo": "https://github.com/carlosferreyra/Portfolio",
-    "name": "Portfolio",
-    "img": "https://opengraph.githubassets.com/a7fee74f5b44f14adf11396677af3cf0dc930a1051e79be3affaf5917ce85f91/carlosferreyra/Portfolio",
-    "description": "My Personal Portfolio",
-    "stack": [
-      "personal-portfolio",
-      "personal-site",
-      "personal-website",
-      "portfolio",
-      "portfolio-site",
-      "portfolio-website"
-    ],
-    "demo": "https://carlosferreyra.github.io/Portfolio/"
-  },
-  {
     "repo": "https://github.com/Seminario-Integrador-2024/GestionTUP",
     "name": "GestionTUP",
     "img": "https://repository-images.githubusercontent.com/839530563/77ec1cfb-4a65-4e34-8641-2fa6d5ea3f18",
